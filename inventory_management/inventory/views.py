4db156b9268a1ac5d7d9237f20e842b5791a4a6b
ItumelengA <202303305@spu.ac.za>
2025-04-08 14:39:01 +0200

    Added success messages for item add, edit, delete with JS alert display

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -3,61 +3,74 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, View, CreateView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .forms import UserRegisterForm, InventoryItemForm
 from .models import InventoryItem, Category
 
+
 class Index(TemplateView):
-	template_name = 'inventory/index.html'
+    template_name = 'inventory/index.html'
 
-class Dashboard(LoginRequiredMixin,View):
-	def get(self,request):
-		items = InventoryItem.objects.filter(user=request.user.id).order_by('id')
 
-		return render(request, 'inventory/dashboard.html',{'items':items})
+class Dashboard(LoginRequiredMixin, View):
+    def get(self, request):
+        items = InventoryItem.objects.filter(user=request.user.id).order_by('id')
+        return render(request, 'inventory/dashboard.html', {'items': items})
+
 
 class SignUpView(View):
-	def get(self, request):
-		form = UserRegisterForm()
-		return render(request, 'inventory/signup.html', {'form': form})
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'inventory/signup.html', {'form': form})
 
-	def post(self, request):
-		form = UserRegisterForm(request.POST)
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            return redirect('index')
+        return render(request, 'inventory/signup.html', {'form': form})
 
-		if form.is_valid():
-			form.save()
-			user = authenticate(
-				username=form.cleaned_data['username'],
-				password=form.cleaned_data['password1']
-			)
-
-			login(request, user)
-			return redirect('index')
-
-		return render(request, 'inventory/signup.html', {'form': form})
 
 class AddItem(LoginRequiredMixin, CreateView):
-	model = InventoryItem
-	form_class = InventoryItemForm
-	template_name = 'inventory/item_form.html'
-	success_url = reverse_lazy('dashboard')
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['categories'] = Category.objects.all()
-		return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
-	def form_valid(self, form):
-		form.instance.user = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "Item has been added successfully!")
+        return super().form_valid(form)
+
 
 class EditItem(LoginRequiredMixin, UpdateView):
-	model = InventoryItem
-	form_class = InventoryItemForm
-	template_name = 'inventory/item_form.html'
-	success_url = reverse_lazy('dashboard')
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/item_form.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Item has been updated successfully!")
+        return super().form_valid(form)
+
 
 class DeleteItem(LoginRequiredMixin, DeleteView):
-	model = InventoryItem
-	template_name = 'inventory/delete_item.html'
-	success_url = reverse_lazy('dashboard')
-	context_object_name = 'item'
+    model = InventoryItem
+    template_name = 'inventory/delete_item.html'
+    success_url = reverse_lazy('dashboard')
+    context_object_name = 'item'
+
+    def form_valid(self, form):
+        messages.success(self.request, "Item has been deleted successfully!")
+        return super().form_valid(form)
+
